bd821cd1bb4c663bff10a1c852b066440c5a4623
Joao Lucas <jldecristo@gmail.com>
2025-07-22 22:12:32 -0300

    metodo totalValue

--- src/entities/HourContract.java
+++ src/entities/HourContract.java
@@ -41,4 +41,9 @@ public class HourContract {
         this.hours = hours;
     }
 
+    public Double totalValue() {
+        double totalValue;
+        return totalValue = hours * valuePerHour;
+    }
+
 }
