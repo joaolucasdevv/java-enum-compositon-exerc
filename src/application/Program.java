18517c808667fb8a5942103d52b9cf7a112198f5
Joao Lucas <jldecristo@gmail.com>
2025-07-24 20:06:11 -0300

    worker

--- src/application/Program.java
+++ src/application/Program.java
@@ -23,6 +23,9 @@ public class Program {
         System.out.println("Base Salary:");
         double baseSalary = sc.nextDouble();
 
+        System.out.println("How many contracts to this worker?");
+        int n = sc.nextInt();
+
 
 
 
